0dc41b789a4c3c61c6c36ef04515c9af29716852
sharjeel-official12 <official.sharjeel1@gmail.com>
2024-08-08 03:20:34 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -2,6 +2,11 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+  console.log(`${req.ip} is saying hello`)
+  res.send('Hello World')
+})
+
 app.get('/water', (req, res) => {
   console.log(`${req.ip} is asking for water`)
   res.send('Here is some water for you')
